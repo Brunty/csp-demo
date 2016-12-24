200a0827a25699a0a43e2dcc5cad0f5888c004b1
brunty <matt@mfyu.co.uk>
2016-12-24 20:32:29 +0000

    Updates project to use dinghy proxy locally

--- app/bootstrap/middleware.php
+++ app/bootstrap/middleware.php
@@ -7,7 +7,10 @@ use App\Middleware\DisableXssProtectionMiddleware;
  * Browsers now implement some built-in XSS protection if they detect it in request attributes.
  *
  * To show some CSP stuff, we'll just disable that here so it doesn't interfere with showing
- * what I want to show.
+ * what I want to show. DO NOT DO THIS ON YOUR SITES.
  */
 $app->add(new DisableXssProtectionMiddleware);
-$app->add(new CspMiddleware($container['app.generator.nonce.generated_nonce']));
+/*
+ * Now let's add our CSP middleware
+ */
+$app->add(new CspMiddleware("default-src 'none'; img-src 'self'; style-src 'self'; script-src 'unsafe-inline'", $container['app.generator.nonce.generated_nonce']));
