e3e52a663483ec918d47e196ccfa5984e7ce550f
brunty <matt@mfyu.co.uk>
2017-04-13 23:40:00 +0100

    Console log stop message

--- app/src/Middleware/CspMiddleware.php
+++ app/src/Middleware/CspMiddleware.php
@@ -29,11 +29,12 @@ class CspMiddleware
     /**
      * Example middleware invokable class
      *
-     * @param  \Psr\Http\Message\ServerRequestInterface $request PSR7 request
+     * @param  \Psr\Http\Message\ServerRequestInterface $request  PSR7 request
      * @param  \Psr\Http\Message\ResponseInterface      $response PSR7 response
-     * @param  callable                                 $next Next middleware
+     * @param  callable                                 $next     Next middleware
      *
      * @return \Psr\Http\Message\ResponseInterface
+     * @throws \InvalidArgumentException
      */
     public function __invoke(
         ServerRequestInterface $request,
@@ -42,7 +43,7 @@ class CspMiddleware
     ) : ResponseInterface
     {
         $newResponse = $response->withAddedHeader(
-            "Content-Security-Policy",
+            'Content-Security-Policy',
             sprintf($this->policy, 'nonce-' . $this->nonce)
         );
 
