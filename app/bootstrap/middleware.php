78fa798a5bad2fd5225bedf74f6ac2f206215d27
brunty <matt@mfyu.co.uk>
2017-05-17 06:37:05 +0100

    Report only middleware setup and comments about risky use

--- app/bootstrap/middleware.php
+++ app/bootstrap/middleware.php
@@ -1,16 +1,22 @@
 <?php
 
 use App\Middleware\CspMiddleware;
+use App\Middleware\CspReportOnlyMiddleware;
 use App\Middleware\DisableXssProtectionMiddleware;
 
 /*
- * Browsers now implement some built-in XSS protection if they detect it in request attributes.
+ * Some browsers now implement some built-in XSS protection if they detect it in request attributes.
  *
- * To show some CSP stuff, we'll just disable that here so it doesn't interfere with showing
- * what I want to show. *DO NOT DO THIS ON YOUR SITES*
+ * To show some CSP stuff, we'll have to disable that here so it doesn't interfere with demonstrating
+ * the things I want you to see. *DO NOT DO THIS ON YOUR SITES*
+ *
+ * I repeat:
+ * DO NOT DO THIS ON YOUR SITES
+ * UNDERSTAND?!
  */
 $app->add(new DisableXssProtectionMiddleware);
 /*
  * Now let's add our CSP middleware
  */
 //$app->add(new CspMiddleware($container['config']['security']['csp'], $container['app.generator.nonce.generated_nonce']));
+//$app->add(new CspReportOnlyMiddleware($container['config']['security']['csp'], $container['app.generator.nonce.generated_nonce']));
