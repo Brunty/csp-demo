8328f9719ec3b86cb44f9e8ef76d88e9c8ae1acf
brunty <matt@mfyu.co.uk>
2017-05-17 06:55:54 +0100

    Comment for documentation in Xss Middleware

--- app/src/Middleware/DisableXssProtectionMiddleware.php
+++ app/src/Middleware/DisableXssProtectionMiddleware.php
@@ -15,16 +15,26 @@ class DisableXssProtectionMiddleware
      * @param  \Psr\Http\Message\ResponseInterface      $response PSR7 response
      * @param  callable                                 $next     Next middleware
      *
+     * @throws \InvalidArgumentException
+     *
      * @return \Psr\Http\Message\ResponseInterface
      */
     public function __invoke(
         ServerRequestInterface $request,
         ResponseInterface $response,
         callable $next
-    ) : ResponseInterface
-    {
+    ): ResponseInterface {
+        /** @var ResponseInterface $response */
         $response = $next($request, $response);
 
+        /*
+         * https://scotthelme.co.uk/hardening-your-http-response-headers/#x-xss-protection
+         *
+         * Possible values:
+         *      0                   disables protection
+         *      1                   enables protection: sanitise script / input before sending to server
+         *      1; mode=block       enables protection: block the response rather than sanitising
+         */
         return $response->withHeader('X-XSS-Protection', '0');
     }
 }
