7bca0efe5a33ab187474cd7d08cd54cac1907034
brunty <matt@mfyu.co.uk>
2017-06-08 18:40:47 +0100

    Added font to demo CSP font and style src

--- app/resources/config/security.php
+++ app/resources/config/security.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-    'csp' => "default-src 'none'; img-src 'self'; style-src 'self' '%1\$s'; script-src 'self' '%1\$s'; upgrade-insecure-requests;"
+    'csp' => "default-src 'none'; img-src 'self'; style-src 'self' '%1\$s' https://*.googleapis.com; font-src https://*.gstatic.com; script-src 'self' '%1\$s'; upgrade-insecure-requests;"
 ];
